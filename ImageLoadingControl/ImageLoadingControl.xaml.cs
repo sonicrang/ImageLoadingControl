8d0612b4e27ede14448039c17e917d45bbc7e322
wurang <sonicrang@gmail.com>
2016-02-16 14:26:24 +0800

    add

---
+++ ImageLoadingControl/ImageLoadingControl.xaml.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Net.Cache;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+
+namespace ImageLoadingControl
+{
+    /// <summary>
+    /// ImageLoadingControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class ImageLoadingControl : UserControl
+    {
+        private Storyboard story;
+        private string path;
+        BackgroundWorker worker = new BackgroundWorker();
+        public ImageLoadingControl()
+        {
+            InitializeComponent();
+            InitializeComponent();
+            story = (base.Resources["waiting"] as Storyboard);
+
+
+            worker.DoWork += (s, e) =>
+            {
+                Uri uri = new Uri(e.Argument.ToString());
+
+                using (WebClient webClient = new WebClient())
+                {
+                    webClient.Proxy = null;  //avoids dynamic proxy discovery delay
+                    webClient.CachePolicy = new RequestCachePolicy(RequestCacheLevel.Default);
+                    try
+                    {
+                        byte[] imageBytes = null;
+
+                        imageBytes = webClient.DownloadData(uri);
+
+                        if (imageBytes == null)
+                        {
+                            e.Result = null;
+                            return;
+                        }
+                        MemoryStream imageStream = new MemoryStream(imageBytes);
+                        BitmapImage image = new BitmapImage();
+
+                        image.BeginInit();
+                        image.StreamSource = imageStream;
+                        image.CacheOption = BitmapCacheOption.OnLoad;
+                        image.EndInit();
+
+                        image.Freeze();
+                        imageStream.Close();
+
+                        e.Result = image;
+                    }
+                    catch (WebException ex)
+                    {
+                        e.Result = null;
+                    }
+                }
+            };
+
+            worker.RunWorkerCompleted += (s, e) =>
+            {
+                try
+                {
+                    BitmapImage bitmapImage = e.Result as BitmapImage;
+                    if (bitmapImage != null)
+                    {
+                        imageShow.Source = bitmapImage;
+                    }
+
+                    worker.Dispose();
+                }
+                catch
+                {
+                    imageShow.Source = null;
+                }
+            };
+        }
+        public string Source
+        {
+            get { return (string)GetValue(SourceProperty); }
+            set { SetValue(SourceProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Source.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SourceProperty =
+            DependencyProperty.Register("Source", typeof(string), typeof(ImageLoadingControl), new FrameworkPropertyMetadata(new PropertyChangedCallback(CallBack)));
+
+        private static void CallBack(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ImageLoadingControl imgCtl = d as ImageLoadingControl;
+            imgCtl.path = e.NewValue != null ? e.NewValue.ToString() : "";
+            imgCtl.GetImage();
+        }
+
+        private void GetImage()
+        {
+            if (!string.IsNullOrEmpty(path))
+            {
+
+                if (path.StartsWith("http"))
+                {
+                    StartLoading();
+                    worker.RunWorkerAsync(path);
+                }
+                else
+                {
+                    imageShow.Source = GetBitmapImage(path);
+                }
+
+            }
+            else
+            {
+                
+            }
+        }
+        private BitmapImage GetBitmapImage(string path, int imageWidth = 0)
+        {
+            BitmapImage bitmap;
+            if (File.Exists(path))
+            {
+                using (MemoryStream ms = new MemoryStream(File.ReadAllBytes(path)))
+                {
+                    bitmap = new BitmapImage();
+                    bitmap.BeginInit();
+
+                    bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                    if (imageWidth > 0)
+                    {
+                        using (System.Drawing.Image drawingImage = System.Drawing.Image.FromStream(ms))
+                        {
+                            int old_w = drawingImage.Width;
+                            int old_h = drawingImage.Height;
+                            int imageHeight = (int)(old_h * (imageWidth * 1.0 / old_w));
+                            using (System.Drawing.Image thumImage = drawingImage.GetThumbnailImage(imageWidth, imageHeight, () => { return true; }, IntPtr.Zero))
+                            {
+                                MemoryStream ms_thum = new MemoryStream();
+                                thumImage.Save(ms_thum, System.Drawing.Imaging.ImageFormat.Png);
+                                bitmap.StreamSource = ms_thum;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        bitmap.StreamSource = ms;
+                    }
+                    bitmap.EndInit();
+                    bitmap.Freeze();
+                }
+
+                return bitmap;
+            }
+            else
+                return null;
+        }
+
+        private void StartLoading()
+        {
+            this.story.Begin();
+            imageLoading.Visibility = Visibility.Visible;
+        }
+        private void StopLoading()
+        {
+            this.story.Stop();
+            imageLoading.Visibility = Visibility.Collapsed;
+        }
+
+    }
+}
---
+++ Test/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Test
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private string imageUrl1;
+
+        public string ImageUrl1
+        {
+            get { return imageUrl1; }
+            set 
+            {
+                imageUrl1 = value;
+                OnPropertyChanged("ImageUrl1");
+            }
+        }
+
+        private string imageUrl2;
+
+        public string ImageUrl2
+        {
+            get { return imageUrl2; }
+            set
+            {
+                imageUrl2 = value;
+                OnPropertyChanged("ImageUrl2");
+            }
+        }
+
+        private string imageUrl3;
+
+        public string ImageUrl3
+        {
+            get { return imageUrl3; }
+            set
+            {
+                imageUrl3 = value;
+                OnPropertyChanged("ImageUrl3");
+            }
+        }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+       
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            ImageUrl1 = @"https://pixabay.com/static/uploads/photo/2016/02/09/13/45/rock-carvings-1189288_960_720.jpg";
+            ImageUrl2 = @"https://pixabay.com/static/uploads/photo/2016/02/14/14/32/construction-1199586_960_720.jpg";
+            ImageUrl3 = @"c:\test.jpg";
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+    }
+}
